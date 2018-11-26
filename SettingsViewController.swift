3f067fc1fe7428bb2a6a66fcd0dd4a4297d5fc1b
Arielle Shnayder <Arielle00@gmail.com>
2018-11-26 12:18:17 -0800

    Add files via upload

---
+++ SettingsViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SettingsViewController.swift
+//  Calculator
+//
+//  Created by Arielle Shnayder on 11/24/18.
+//  Copyright © 2018 Arielle Shnayder. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var setTipController: UISegmentedControl!
+    
+    
+    
+    @IBAction func changeController(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func changed(_ sender: Any) {
+        
+    }
+    
+        
+        // Do any additional setup after loading the view.
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
